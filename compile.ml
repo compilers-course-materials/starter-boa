5e66f57f96ad89eca349c0e9c6b8cf37adfb0765
Joe Gibbs Politz <joe.politz@gmail.com>
2016-02-10 07:35:46 -0500

    .

--- compile.ml
+++ compile.ml
@@ -1,8 +1,8 @@
 open Printf
 
 type reg =
-	| EAX
-	| ESP
+  | EAX
+  | ESP
 
 type arg =
   | Const of int
@@ -10,15 +10,16 @@ type arg =
   | RegOffset of int * reg
 
 type instruction =
-	| IMov of arg * arg
+  | IMov of arg * arg
   | IAdd of arg * arg
   | ISub of arg * arg
   | IMul of arg * arg
   | ILabel of string
   | ICmp of arg * arg
   | IJne of string
+  | IJe of string
   | IJmp of string
-	| IRet
+  | IRet
 
 type prim1 =
   | Add1
@@ -38,29 +39,29 @@ type expr =
   | EId of string
 
 
-type aimmediate =
-  | ANumber of int
-  | AId of string
+type immexpr =
+  | ImmNumber of int
+  | ImmId of string
 
-and astep =
-  | APrim1 of prim1 * aimmediate
-  | APrim2 of prim2 * aimmediate * aimmediate
-  | AIf of aimmediate * aexpr * aexpr
-  | AImmediate of aimmediate
+and cexpr =
+  | CPrim1 of prim1 * immexpr
+  | CPrim2 of prim2 * immexpr * immexpr
+  | CIf of immexpr * aexpr * aexpr
+  | CImmExpr of immexpr
 
 and aexpr =
-  | ALet of string * astep * aexpr
-  | AStep of astep
+  | ALet of string * cexpr * aexpr
+  | ACExpr of cexpr
 
 let count = ref 0
 let gen_temp base =
   count := !count + 1;
   sprintf "temp_%s_%d" base !count
 
-let rec anf e (k : aimmediate -> aexpr) =
+let rec anf e (k : immexpr -> aexpr) =
   match e with
     | EPrim1(op, e) ->
-      anf e (fun imm -> AStep(APrim1(op, imm)))
+      anf e (fun imm -> ACExpr(CPrim1(op, imm)))
     | ELet([], body) ->
       failwith "You need to do this one (empty let)"
     | ELet((name, value)::rest, body) ->
@@ -70,14 +71,14 @@ let rec anf e (k : aimmediate -> aexpr) =
     | EIf(cond, thn, els) ->
       failwith "You need to do this one (if)"
     | ENumber(n) ->
-      k(ANumber(n))
+      k(ImmNumber(n))
     | EId(name) ->
       failwith "You need to do this one (id)"
 
 let r_to_asm (r : reg) : string =
-	match r with
-		| EAX -> "eax"
-		| ESP -> "esp"
+  match r with
+    | EAX -> "eax"
+    | ESP -> "esp"
 
 let arg_to_asm (a : arg) : string =
   match a with
@@ -90,25 +91,27 @@ let arg_to_asm (a : arg) : string =
         sprintf "[%s-%d]" (r_to_asm r) (-1 * n)
 
 let i_to_asm (i : instruction) : string =
-	match i with
-		| IMov(dest, value) ->
-			sprintf "  mov %s, %s" (arg_to_asm dest) (arg_to_asm value)
-		| IAdd(dest, to_add) ->
-			sprintf "  add %s, %s" (arg_to_asm dest) (arg_to_asm to_add)
-		| ISub(dest, to_sub) ->
-			sprintf "  sub %s, %s" (arg_to_asm dest) (arg_to_asm to_sub)
-		| IMul(dest, to_mul) ->
-			sprintf "  imul %s, %s" (arg_to_asm dest) (arg_to_asm to_mul)
+  match i with
+    | IMov(dest, value) ->
+      sprintf "  mov %s, %s" (arg_to_asm dest) (arg_to_asm value)
+    | IAdd(dest, to_add) ->
+      sprintf "  add %s, %s" (arg_to_asm dest) (arg_to_asm to_add)
+    | ISub(dest, to_sub) ->
+      sprintf "  sub %s, %s" (arg_to_asm dest) (arg_to_asm to_sub)
+    | IMul(dest, to_mul) ->
+      sprintf "  imul %s, %s" (arg_to_asm dest) (arg_to_asm to_mul)
     | ICmp(left, right) ->
       failwith "You need to do this one (cmp)"
     | ILabel(name) ->
       failwith "You need to do this one (label)"
     | IJne(label) ->
       failwith "You need to do this one (jne)"
+    | IJe(label) ->
+      failwith "You need to do this one (jne)"
     | IJmp(label) ->
       failwith "You need to do this one (jmp)"
-		| IRet ->
-			"	ret"
+    | IRet ->
+      " ret"
 
 let to_asm (is : instruction list) : string =
   List.fold_left (fun s i -> sprintf "%s\n%s" s (i_to_asm i)) "" is
@@ -119,19 +122,19 @@ let rec find ls x =
     | (y,v)::rest ->
       if y = x then Some(v) else find rest x
 
-let acompile_imm_arg (i : aimmediate) _ (env : (string * int) list) : arg =
+let acompile_imm_arg (i : immexpr) _ (env : (string * int) list) : arg =
   match i with
-    | ANumber(n) -> Const(n)
-    | AId(name) ->
+    | ImmNumber(n) -> Const(n)
+    | ImmId(name) ->
       failwith "You need to do this one (compiled id)"
 
-let acompile_imm (i : aimmediate) (si : int) (env : (string * int) list) : instruction list =
+let acompile_imm (i : immexpr) (si : int) (env : (string * int) list) : instruction list =
   [ IMov(Reg(EAX), acompile_imm_arg i si env) ]
 
-let rec acompile_step (s : astep) (si : int) (env : (string * int) list) : instruction list =
+let rec acompile_step (s : cexpr) (si : int) (env : (string * int) list) : instruction list =
   match s with
-    | AImmediate(i) -> acompile_imm i si env
-    | APrim1(op, e) ->
+    | CImmExpr(i) -> acompile_imm i si env
+    | CPrim1(op, e) ->
       let prelude = acompile_imm e si env in
       begin match op with
         | Add1 ->
@@ -143,28 +146,29 @@ let rec acompile_step (s : astep) (si : int) (env : (string * int) list) : instr
             IAdd(Reg(EAX), Const(-1))
           ]
       end
-    | APrim2(op, left, right) ->
+    | CPrim2(op, left, right) ->
       failwith "You need to do this one (compile prim2)"
-    | AIf(cond, thn, els) ->
+    | CIf(cond, thn, els) ->
       failwith "You need to do this one (compile if)"
 
 and acompile_expr (e : aexpr) (si : int) (env : (string * int) list) : instruction list =
-	match e with
+  match e with
     | ALet(id, e, body) ->
       let prelude = acompile_step e (si + 1) env in
       let body = acompile_expr body (si + 1) ((id, si)::env) in
       prelude @ [
         IMov(RegOffset(-4 * si, ESP), Reg(EAX))
       ] @ body
-    | AStep(s) -> acompile_step s si env
+    | ACExpr(s) -> acompile_step s si env
 
 let compile_to_string prog =
+  count := 0
   let prelude =
     "section .text
 global our_code_starts_here
 our_code_starts_here:" in
-  let anfed = (anf prog (fun i -> AStep(AImmediate(i)))) in
+  let anfed = (anf prog (fun i -> ACExpr(CImmExpr(i)))) in
   let compiled = (acompile_expr anfed 1 []) in
   let as_assembly_string = (to_asm (compiled @ [IRet])) in
-	sprintf "%s%s\n" prelude as_assembly_string
+  sprintf "%s%s\n" prelude as_assembly_string
 
